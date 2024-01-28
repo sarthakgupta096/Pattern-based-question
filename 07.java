044e345d8144720b0deb1840d1057fa63d30ac40
Sarthak Gupta <118627778+sarthakgupta096@users.noreply.github.com>
2024-01-28 22:30:36 +0530

    07.java

--- 07.java
+++ 07.java
@@ -1,1 +1,41 @@
 
+
+//Problem statement
+Ninja was very fond of patterns. For a given integer ‘N’, he wants to make the N-Star Triangle.
+
+Example:
+Input: ‘N’ = 3
+
+Output: 
+
+  *
+ ***
+*****
+
+ //solution
+
+    public class Solution {
+    public static void nStarTriangle(int n) {
+        // Write your code here
+        for(int i=0;i<n;i++)
+        {
+
+            for(int j=0;j<n-i-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i+1;j++)
+            {
+                System.out.print("*");
+            }
+            for(int j=0;j<n-i-1;j++)
+            {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+
+        }
+        
+    }
+}
